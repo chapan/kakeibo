da180d2e9e709f4207c87562789b6d4ecdb3c02d
chapan <chapan_G@hotmail.com>
2018-08-22 17:42:00 +0900

    微調整

--- app/controllers/twitter/tweets_controller.rb
+++ app/controllers/twitter/tweets_controller.rb
@@ -6,14 +6,20 @@ class Twitter::TweetsController < BaseController
 
   end
 
-  # 課金額を集計してプロフィールを更新する
+  # 課金額を集計してプロフィールを更新する(手動更新用)
   def update_profile
     charge_lv, charge_exp = calc_status
     name = Settings.twitter.name_base + charge_lv.to_s
     description = Settings.twitter.description_base + (REQUIRE_EXP - charge_exp).to_s
 
     begin
-      TweetManager.new.update_profile(name: name, description: description)
+      twitter = TweetManager.new
+
+      # プロフィール更新
+      twitter.update_profile(name: name, description: description)
+      # ツイート
+      twitter.tweet("現在のレベル #{charge_lv}\n 次のレベルまで #{charge_exp}")
+
       redirect_to :kakeis, flash: {notice: "ツイッターを更新しました。"}
     rescue => ex
       redirect_to :kakeis, flash: {alert: "エラーが発生しました。\n #{ex.message}"}
